77ec0ee5404f468010b6d5d804815db0b177859e
Lukas Horak <lukasbhorak@gmail.com>
2019-06-05 15:14:56 -0400

    added helper functions to tinyAppTools

--- tinyAppTools.js
+++ tinyAppTools.js
@@ -1,11 +1,30 @@
 const crypto = require("crypto");
 
 module.exports = {
-  generateRandomString: (length) => {
+  generateRandomString: function (length) {
     if (length > 50 || length < 0 || length % 1 !== 0) {
       throw new RangeError("generateRandomString() must take a positive integer, and maxes out and a length of 50 characters!")
     }
     const randString = crypto.randomBytes(25).toString('hex');
     return randString.substring(0, length)
+  },
+  idExists: function (resource, id) {
+    return resource.hasOwnProperty(id)
+  },
+  generateUniqueId: function(resource, idLength) {
+    let id;
+    do{
+      id = this.generateRandomString(idLength)
+    }
+    while (this.idExists(resource, id));
+    return id;
+  },
+  propertyIsTaken: function(property, resource, newProp){
+    for(let i in resource){
+      if (resource[i][property] === newProp){
+        return true;
+      }
+    }
+    return false;
   }
 }
